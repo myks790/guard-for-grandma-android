22efdea7a773dce2459c22eaeed808295bfedf3a
SangHun Kang <myks790@gmail.com>
2020-10-18 12:08:10 +0900

    report 기능 추가

--- app/src/main/java/com/example/guardforgrandma/CheckService.java
+++ app/src/main/java/com/example/guardforgrandma/CheckService.java
@@ -6,6 +6,8 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.BatteryManager;
 import android.os.IBinder;
 import android.util.Log;
@@ -13,6 +15,13 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -29,13 +38,12 @@ public class CheckService extends Service {
         super.onCreate();
         String channelId = "com.guardGrandma";
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId, "guard for grandma channel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel = new NotificationChannel(channelId, "guard for grandma report channel", NotificationManager.IMPORTANCE_DEFAULT);
             NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             manager.createNotificationChannel(channel);
         }
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId);
-        builder.setContentText("test");
 
         final IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         TimerTask timerTask = new TimerTask() {
@@ -46,13 +54,43 @@ public class CheckService extends Service {
                     int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
                     int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
                     float batteryPct = level * 100 / (float)scale;
-                    Log.i("tag", "battery : "+batteryPct);
+                    report("battery", String.valueOf(batteryPct));
                 }
+                Context context = getApplicationContext();
+                ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+                NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+                boolean isWiFi = activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
+                report("wifi", isWiFi +"");
             }
         };
         timer = new Timer();
-        timer.schedule(timerTask,0,10000);
+        timer.schedule(timerTask,0,10*60*1000);
         startForeground(1, builder.build());
+
+    }
+
+    private void report(String title, String data) {
+        URL url = null;
+        try {
+            url = new URL("http://myks790.iptime.org:41086/report");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setConnectTimeout(5000);
+
+            try {
+                DataOutputStream stream = new DataOutputStream(connection.getOutputStream());
+                stream.writeBytes("title="+title+"&data="+data);
+                stream.flush();
+                stream.close();
+                int responseCode = connection.getResponseCode();
+            } finally {
+                connection.disconnect();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
